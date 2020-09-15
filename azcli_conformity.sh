800dd5c83edfe77579928cffe59d79f25837b5cc
Tom Ryan <25472582+TomRyan-321@users.noreply.github.com>
2020-09-15 22:32:35 +1000

    Grant Admin Consent, Add Whitespace Support, Creation of Custom Role as required by latest conformity policy (#2)
    # Changelog :tada:
    
    - Support whitespace in name and grant admin permissions;
    -  Creation of custom role and assignment to subscriptions;

--- azcli_conformity.sh
+++ azcli_conformity.sh
@@ -1,24 +1,27 @@
 # Define Variables for the Configurations
 export APP_SECRET=$(uuidgen)
-export APP_SECRET_DESCRIPTION=secret
 
 # Ask User Input for the App Registration Name
 echo " "
-echo 'Which name do you want to give to your App Registration?'
-read APP_NAME
+echo "Which name do you want to give to your App Registration?"
+IFS=$'\n'
+read -r APP_NAME
 echo " "
 echo "Hello, Thank you for trying this script out.
 We're configuring your Azure Account to work with Cloud One Conformity, this might take several minutes depending of how many subscriptions you have."
 
 # Create App Registration
-az ad app create --display-name $APP_NAME --password $APP_SECRET --credential-description $APP_SECRET_DESCRIPTION --required-resource-accesses @manifest.json > /dev/null
+az ad app create --display-name "$APP_NAME" --password $APP_SECRET --credential-description secret --required-resource-accesses @manifest.json > /dev/null
 
 # Export App Registration ID and Active Directory ID
 export AD_ID=`az account show --query tenantId --output tsv`
-export APPID=`az ad app list --display-name $APP_NAME | grep appId | cut -d ":" -f2 | grep -o '".*"' | sed 's/^"\(.*\)".*/\1/'`
+export APPID=`az ad app list --display-name "$APP_NAME" | grep appId | cut -d ":" -f2 | grep -o '".*"' | sed 's/^"\(.*\)".*/\1/'`
 
 # Create a Service Principal for the App Registration
 az ad sp create --id $APPID > /dev/null
+
+#Grant Admin Consent to application
+az ad app permission admin-consent --id $APPID
 
 # Export All Subscriptions ID's to an Array and Count to a Variable
 az account list --refresh > /dev/null
@@ -29,8 +32,23 @@ NUM_SUBS=${#SUBSCRIPTIONS[@]}
  i=0
  for (( i; i<=$NUM_SUBS; i++ ))
  do  
-    az role assignment create --role Reader --assignee $APPID --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
-    sleep 5
+    az role definition create --role-definition '{ \
+      "Name": "Custom Role - Cloud One Conformity", \
+      "Description": "Subscription level custom role for Cloud Conformity access.", \
+      "Actions": [ \
+         "Microsoft.AppConfiguration/configurationStores/ListKeyValue/action", \
+         "Microsoft.Network/networkWatchers/queryFlowLogStatus/action", \
+         "Microsoft.Web/sites/config/list/Action", \
+         "Microsoft.Storage/storageAccounts/queueServices/queues/read" \
+      ], \
+      "DataActions": [], \
+      "NotDataActions": [], \
+      "AssignableScopes": ["/subscriptions/'${SUBSCRIPTIONS[i]}'"] \
+      }' > /dev/null
+   sleep 5
+   az role assignment create --role "Custom Role - Cloud One Conformity" --assignee $APPID --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
+   sleep 5
+   az role assignment create --role Reader --assignee $APPID --scope /subscriptions/${SUBSCRIPTIONS[i]} > /dev/null
  done
 
 # Print All the Information
@@ -38,5 +56,5 @@ echo "Here the information that you'll need to use to finish the integration"
 echo " "
 echo 'Active Directory ID: '$AD_ID
 echo 'Application ID: '$APPID
-echo 'Application Secret :'$APP_SECRET
+echo 'Application Secret: '$APP_SECRET
 echo " "
